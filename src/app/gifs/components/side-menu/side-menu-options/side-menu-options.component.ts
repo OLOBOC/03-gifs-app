9602f3a90a47df4dcd347f4b58d1dac798f3334f
OLOBOC <omarlobocuesta05@gmail.com>
2025-04-23 11:05:49 +0200

    routerLinks

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,8 +1,36 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  router: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent {
+
+  menuOptions: MenuOption[] = [
+
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      router: '/dashboard/trending',
+      subLabel: 'gifs populares',
+    },
+    {
+      icon: 'fa-solid fa-mangnifying-glass',
+      label: 'Buscador',
+      router: '/dashboard/search',
+      subLabel: 'Buscar gifs',
+    },
+
+  ]
+}
