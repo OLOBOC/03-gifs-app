bf3ce7745eabd9e1bb28db6bfd21370b254775d9
OLOBOC <omarlobocuesta05@gmail.com>
2025-04-23 09:04:27 +0200

    creadas carpetas trending y search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+
+
+
+
+export const routes: Routes = [
+
+
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component')
+
+  },
+  {
+    path:'trending',
+    redirectTo: './gifs/pages/trending-page/trending-page.component',
+  },
+  {
+    path:'search',
+    redirectTo: './gifs/pages/search-page/search-page.component',
+  },
+  {
+    path:'**',
+    redirectTo: 'dashboard',
+  }
+];
---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [],
+  templateUrl: './dashboard-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class DashboardPageComponent {
+
+}
